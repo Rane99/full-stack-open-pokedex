799759f3424ea22ef0cb9959e8441e61ab7cae04
Joona Rantanen <joona.rantanen1@gmail.com>
2021-05-16 00:53:07 +0300

    add second test

--- cypress/integration/first_test.js
+++ cypress/integration/first_test.js
@@ -6,4 +6,9 @@ describe('Pokedex', function() {
       cy.contains('ivysaur')
       cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
+    it('navigation from frontpage works', function() {
+        cy.visit('http://localhost:8080')
+        cy.contains('ivysaur').click()
+        cy.contains('chlorophyll ')
+      })
   })
