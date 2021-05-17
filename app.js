25c7fe39511f79d66b531eb6a94ab26e14071cf2
Joona Rantanen <joona.rantanen1@gmail.com>
2021-05-17 17:48:08 +0300

    fix lint errorr

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.get('/health', (req, res) => {
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log('server started on port 5000')
- 
   /* eslint-enable no-console */
 })
 
