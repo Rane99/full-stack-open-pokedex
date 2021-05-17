912ca0567ae9529955cb9ecc7e428dcdff93896e
Joona Rantanen <joona.rantanen1@gmail.com>
2021-05-17 17:43:52 +0300

    add health check

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  console.log("OK")
+  res.send('ok')
+})
+
+
+
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log('server started on port 5000')
+  console.log("----------")
+  console.log("----------")
+  console.log("----------")
+  console.log("----------")
+  console.log("----------")
   /* eslint-enable no-console */
 })
+
